92b103b4bd918e3f60210c82b86683af92cd8df3
Anders Nygren <anders.nygren@gmail.com>
2010-12-06 18:55:11 -0600

    Add decoding of MO-ForwardSM-Arg and MO-ForwardSM-Res.

--- src/pran_map.erl
+++ src/pran_map.erl
@@ -19,11 +19,19 @@
 %%--------------------------------------------------------------------
 decode(invoke,{local,2},Bin) ->
     asn1rt:decode('MAP-MS-DataTypes-v6','UpdateLocationArg',Bin);
+decode(returnResult,{local,2},Bin) ->
+    asn1rt:decode('MAP-MS-DataTypes-v6','UpdateLocationRes',Bin);
+decode(invoke,{local,46},Bin) ->
+    asn1rt:decode('MAP-SM-DataTypes-v6','MO-ForwardSM-Arg',Bin);
+decode(returnResult,{local,46},Bin) ->
+    asn1rt:decode('MAP-SM-DataTypes-v6','MO-ForwardSM-Res',Bin);
 decode(invoke,{local,56},Bin) ->
     asn1rt:decode('MAP-MS-DataTypes-v6','SendAuthenticationInfoArg',Bin);
 decode(returnResult,{local,56},Bin) ->
     asn1rt:decode('MAP-MS-DataTypes-v6','SendAuthenticationInfoRes',Bin);
-decode(_TCop,_Op,Bin) ->
+decode(TCop,Op,Bin) ->
+    error_logger:info_report([{module,?MODULE},
+			      {do_not_know_how_to_decode, TCop,Op}]),
     Bin.
     
 %%====================================================================

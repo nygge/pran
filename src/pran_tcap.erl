9fc0bf78194d31a63597e8e9e314153c6e76b658
Anders Nygren <anders.nygren@gmail.com>
2010-12-07 12:56:14 -0600

    TCAP uses configuration data to decide how to decode component parameters.

--- src/pran_tcap.erl
+++ src/pran_tcap.erl
@@ -10,8 +10,13 @@
 %% API
 -export([decode/2]).
 
+%% Dummy function used when no decoder is found
+-export([nothing/2]).
+
 -include_lib("tcap/include/DialoguePDUs.hrl").
 -include_lib("tcap/include/TCAPMessagesBasic.hrl").
+
+-record(operation, {ac, name, op_code, module, function, arg, res}).
 
 %%====================================================================
 %% API
@@ -21,36 +26,36 @@
 %% Description:
 %%--------------------------------------------------------------------
 decode(Bin,Opts) ->
-    {ok,Rec}=asn1rt:decode('TCAPMessagesBasic','TCMessage',Bin),
-    decode1(Rec).
+    {ok,Rec}='TCAPMessagesBasic':decode('TCMessage',Bin),
+    decode1(Rec, Opts).
 
 %%====================================================================
 %% Internal functions
 %%====================================================================
-decode1({'begin',#'Begin'{components=asn1_NOVALUE}=B}) ->
+decode1({'begin',#'Begin'{components=asn1_NOVALUE}=B}, Opts) ->
     B;
 decode1({'begin',#'Begin'{dialoguePortion=DP,
-			  components=Cs}=B}) when is_list(Cs) ->
+			  components=Cs}=B},
+	Opts) when is_list(Cs) ->
     Dlg = decode_dialoguePortion(DP),
     AC = get_ac(Dlg),
-    Decoder = ac_to_decoder(AC,[]),
-    Cs1 = [decode_component(C,Decoder) || C<-Cs],
+    Cs1 = [decode_component(AC,C,Opts) || C<-Cs],
     B#'Begin'{dialoguePortion=Dlg,components=Cs1};
 
 decode1({'continue',#'Continue'{dialoguePortion=DP,
-				components=Cs}=C}) when is_list(Cs) ->
+				components=Cs}=C}, 
+	Opts) when is_list(Cs) ->
     Dlg = decode_dialoguePortion(DP),
     AC = get_ac(Dlg),
-    Decoder = ac_to_decoder(AC,[]),
-    Cs1 = [decode_component(C,Decoder) || C<-Cs],
+    Cs1 = [decode_component(AC,C,Opts) || C<-Cs],
     C#'Continue'{dialoguePortion=Dlg,components=Cs1};
-decode1({abort,#'Abort'{}=A}) ->
+decode1({abort,#'Abort'{}=A}, Opts) ->
     A;
-decode1({'end',#'End'{}=E}) ->
+decode1({'end',#'End'{}=E}, Opts) ->
     E.
 
 decode_dialoguePortion({'EXTERNAL',{syntax,?'dialogue-as-id'},_,Bin}) ->
-    {ok,{_Alt,Value}} = asn1rt:decode('DialoguePDUs','DialoguePDU',Bin),
+    {ok,{_Alt,Value}} = 'DialoguePDUs':decode('DialoguePDU',Bin),
     Value;
 decode_dialoguePortion(asn1_NOVALUE) ->
     asn1_NOVALUE.
@@ -62,28 +67,40 @@ get_ac(#'AARE-apdu'{'application-context-name'=AC}) ->
 get_ac(asn1_NOVALUE) ->
     asn1_NOVALUE.
 
-ac_to_decoder(AC,Opts) ->
-    proplists:get_value(AC, Opts, pran_map).
-	
-
-decode_component({invoke,#'Invoke'{operationCode=OpCode,parameter=Param}=I},
-		  Decoder) ->
-    Result = case Decoder:decode(invoke,OpCode,Param) of
+decode_component(AC,{invoke,#'Invoke'{operationCode=OpCode,parameter=Param}=I},
+		 Opts) ->
+    #operation{module=Mod,function=Fun,arg=Type} = find_decoder(AC,OpCode,Opts),
+    Result = case Mod:Fun(Type,Param) of
 		 {ok,Arg} -> Arg;
 		 Error -> Error
 	     end,
     I#'Invoke'{parameter={map,Result}};
-decode_component({returnResultLast,
+decode_component(AC,{returnResultLast,
 		  #'ReturnResult'{
 		    result=#'ReturnResult_result'{operationCode=OpCode,
 						  parameter=Param}=R}=I},
-		 Decoder) ->
-    Result = case Decoder:decode(returnResult,OpCode,Param) of
+		 Opts) ->
+    #operation{module=Mod,function=Fun,arg=Type} = find_decoder(AC,OpCode,Opts),
+    Result = case Mod:Fun(Type,Param) of
 		 {ok,Arg} -> Arg;
 		 Error -> Error
 	     end,
     I#'ReturnResult'{result=R#'ReturnResult_result'{parameter={map,Result}}};
-decode_component({returnResultLast,
-		  #'ReturnResult'{result=asn1_NOVALUE}=I},_Decoder) ->
+decode_component(AC,{returnResultLast,
+		  #'ReturnResult'{result=asn1_NOVALUE}=I},_Opts) ->
     I.
 
+find_decoder(AC,OpCode,Opts) ->
+    Ops = pran_utils:get_conf_par(tcap,operations,Opts),
+    case [Op || #operation{ac=AC1,op_code=Op1}=Op <- Ops, AC==AC1,Op1==OpCode] of
+	[] ->
+	    ORec = #operation{ac=AC,op_code=OpCode,
+			      module=?MODULE,function=nothing},
+	    ORec;
+	[#operation{}=ORec] ->
+	    ORec
+    end.
+
+%% Dummy function used when no decoder is found
+nothing(_Type,Bin) ->
+    Bin.

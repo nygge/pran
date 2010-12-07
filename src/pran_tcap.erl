270f0b48f70b82fdf82895dd11680c7ec36f6408
Anders Nygren <anders.nygren@gmail.com>
2010-12-06 18:53:28 -0600

    Improve decoding of Begin and Continue.

--- src/pran_tcap.erl
+++ src/pran_tcap.erl
@@ -10,6 +10,7 @@
 %% API
 -export([decode/2]).
 
+-include_lib("tcap/include/DialoguePDUs.hrl").
 -include_lib("tcap/include/TCAPMessagesBasic.hrl").
 
 %%====================================================================
@@ -30,28 +31,44 @@ decode1({'begin',#'Begin'{components=asn1_NOVALUE}=B}) ->
     B;
 decode1({'begin',#'Begin'{dialoguePortion=DP,
 			  components=Cs}=B}) when is_list(Cs) ->
-    Dlg=decode_dialoguePortion(DP),
-    Cs1 = [decode_component(C) || C<-Cs],
+    Dlg = decode_dialoguePortion(DP),
+    AC = get_ac(Dlg),
+    Decoder = ac_to_decoder(AC,[]),
+    Cs1 = [decode_component(C,Decoder) || C<-Cs],
     B#'Begin'{dialoguePortion=Dlg,components=Cs1};
 
 decode1({'continue',#'Continue'{dialoguePortion=DP,
 				components=Cs}=C}) when is_list(Cs) ->
-    Dlg=decode_dialoguePortion(DP),
-    Cs1 = [decode_component(C) || C<-Cs],
+    Dlg = decode_dialoguePortion(DP),
+    AC = get_ac(Dlg),
+    Decoder = ac_to_decoder(AC,[]),
+    Cs1 = [decode_component(C,Decoder) || C<-Cs],
     C#'Continue'{dialoguePortion=Dlg,components=Cs1};
 decode1({abort,#'Abort'{}=A}) ->
     A;
 decode1({'end',#'End'{}=E}) ->
     E.
 
-decode_dialoguePortion({'EXTERNAL',{syntax,Syntax},_,Bin}) ->
-    {ok,R} = asn1rt:decode('DialoguePDUs','DialoguePDU',Bin),
-    R;
+decode_dialoguePortion({'EXTERNAL',{syntax,?'dialogue-as-id'},_,Bin}) ->
+    {ok,{_Alt,Value}} = asn1rt:decode('DialoguePDUs','DialoguePDU',Bin),
+    Value;
 decode_dialoguePortion(asn1_NOVALUE) ->
     asn1_NOVALUE.
 
-decode_component({invoke,#'Invoke'{operationCode=OpCode,parameter=Param}=I}) ->
-    Result = case pran_map:decode(invoke,OpCode,Param) of
+get_ac(#'AARQ-apdu'{'application-context-name'=AC}) ->
+    AC;
+get_ac(#'AARE-apdu'{'application-context-name'=AC}) ->
+    AC;
+get_ac(asn1_NOVALUE) ->
+    asn1_NOVALUE.
+
+ac_to_decoder(AC,Opts) ->
+    proplists:get_value(AC, Opts, pran_map).
+	
+
+decode_component({invoke,#'Invoke'{operationCode=OpCode,parameter=Param}=I},
+		  Decoder) ->
+    Result = case Decoder:decode(invoke,OpCode,Param) of
 		 {ok,Arg} -> Arg;
 		 Error -> Error
 	     end,
@@ -59,12 +76,14 @@ decode_component({invoke,#'Invoke'{operationCode=OpCode,parameter=Param}=I}) ->
 decode_component({returnResultLast,
 		  #'ReturnResult'{
 		    result=#'ReturnResult_result'{operationCode=OpCode,
-						  parameter=Param}=R}=I}) ->
-    Result = case pran_map:decode(returnResult,OpCode,Param) of
+						  parameter=Param}=R}=I},
+		 Decoder) ->
+    Result = case Decoder:decode(returnResult,OpCode,Param) of
 		 {ok,Arg} -> Arg;
 		 Error -> Error
 	     end,
     I#'ReturnResult'{result=R#'ReturnResult_result'{parameter={map,Result}}};
 decode_component({returnResultLast,
-		  #'ReturnResult'{result=asn1_NOVALUE}=I}) ->
+		  #'ReturnResult'{result=asn1_NOVALUE}=I},_Decoder) ->
     I.
+

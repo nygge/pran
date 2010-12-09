e57445a082dc94d7bbf334b15ccebb19cf363e42
Anders Nygren <anders.nygren@gmail.com>
2010-12-09 12:17:30 -0600

    Add SDP.

---
+++ src/pran_sdp.erl
@@ -0,0 +1,34 @@
+%%%-------------------------------------------------------------------
+%%% File    : pran_sip.erl
+%%% Author  : Anders Nygren <anders.nygren@gmail.com>
+%%% Description : SDP decoder for PRAN.
+%%%
+%%% Created :  8 Dec 2010 by Anders Nygren <>
+%%%-------------------------------------------------------------------
+-module(pran_sdp).
+
+%% API
+-export([decode/3]).
+
+%%====================================================================
+%% API
+%%====================================================================
+%%--------------------------------------------------------------------
+%% Function: 
+%% Description:
+%%--------------------------------------------------------------------
+decode(Payload, Stack, _Opts) ->
+    try rfc4566:decode('session-description',Payload) of
+	{ok, PDU, <<>>} -> 
+	    {[{sdp,PDU}|Stack],<<>>,done};
+	fail ->
+	    {[{sip,Payload}|Stack],<<>>,done}
+    catch
+	_:Reason ->
+	    io:format("SDP failed ~p~n",[Reason]),
+	    {[{sdp_failed,Payload}|Stack],<<>>,done}
+    end.
+
+%%====================================================================
+%% Internal functions
+%%====================================================================
